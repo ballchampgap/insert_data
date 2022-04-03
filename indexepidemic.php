227e55b254682c3eb1f11b71c03c10078e5248ed
ballchampgap <98090051+ballchampgap@users.noreply.github.com>
2022-04-03 23:27:58 +0700

    olrgjr

--- indexepidemic.php
+++ indexepidemic.php
@@ -81,7 +81,6 @@ $query = mysqli_query($conn, $sql);
             </div>
         </div>
     </div>
-    <button id="btnSend">Send Message</button>
     <script> 
     window.onload = function() {
         var lat, lon = null;
