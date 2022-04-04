73bdd90cec4974ab84f16b1c51f836395d8e74b9
ballchampgap <98090051+ballchampgap@users.noreply.github.com>
2022-04-04 23:53:12 +0700

    o[ujpwerg

--- indexepidemic.php
+++ indexepidemic.php
@@ -99,21 +99,36 @@ $query = mysqli_query($conn, $sql);
     </script>
 
 <script> 
-    function runApp() {
-        liff.getProfile().then(profile => {
-            document.getElementById("pictureUrl").src = profile.pictureUrl;
-            document.getElementById("displayName").innerHTML = '<b>ชื่อผู้แจ้ง:</b> ' + profile.displayName;
-            document.getElementsByName("pname")[0].value = profile.displayName;
-            document.getElementsByName("id_user")[0].value = profile.userId;
-        }).catch(err => console.error(err));
+function runApp() {
+    liff.getProfile().then(profile => {
+        document.getElementById("pictureUrl").src = profile.pictureUrl;
+        document.getElementById("displayName").innerHTML = '<b>ชื่อผู้แจ้ง:</b> ' + profile.displayName;
+        document.getElementsByName("pname")[0].value = profile.displayName;
+        document.getElementsByName("id_user")[0].value = profile.userId;
+    }).catch(err => console.error(err));
+}
+liff.init({ liffId: "1656976284-2geMlQqP" }, () => {
+    if (liff.isLoggedIn()) {
+        runApp()
+    } else {
+        liff.login();
     }
-    liff.init({ liffId: "1656976284-4b86kPOB" }, () => {
-        if (liff.isLoggedIn()) {
-            runApp()
-        } else {
-            liff.login();
-        }
-    }, err => console.error(err.code, error.message));
+}, err => console.error(err.code, error.message));
+    // function runApp() {
+    //     liff.getProfile().then(profile => {
+    //         document.getElementById("pictureUrl").src = profile.pictureUrl;
+    //         document.getElementById("displayName").innerHTML = '<b>ชื่อผู้แจ้ง:</b> ' + profile.displayName;
+    //         document.getElementsByName("pname")[0].value = profile.displayName;
+    //         document.getElementsByName("id_user")[0].value = profile.userId;
+    //     }).catch(err => console.error(err));
+    // }
+    // liff.init({ liffId: "1656976284-4b86kPOB" }, () => {
+    //     if (liff.isLoggedIn()) {
+    //         runApp()
+    //     } else {
+    //         liff.login();
+    //     }
+    // }, err => console.error(err.code, error.message));
     
 const btnSend = document.getElementById("btnSend")
 async function sendMsg() {
--- indexpest.php
+++ indexpest.php
@@ -101,20 +101,35 @@ $query = mysqli_query($conn, $sql);
 
     <script> 
     function runApp() {
-        liff.getProfile().then(profile => {
-            document.getElementById("pictureUrl").src = profile.pictureUrl;
-            document.getElementById("displayName").innerHTML = '<b>ชื่อผู้แจ้ง:</b> ' + profile.displayName;
-            document.getElementsByName("pname")[0].value = profile.displayName;
-            document.getElementsByName("id_user")[0].value = profile.userId;
-        }).catch(err => console.error(err));
+    liff.getProfile().then(profile => {
+        document.getElementById("pictureUrl").src = profile.pictureUrl;
+        document.getElementById("displayName").innerHTML = '<b>ชื่อผู้แจ้ง:</b> ' + profile.displayName;
+        document.getElementsByName("pname")[0].value = profile.displayName;
+        document.getElementsByName("id_user")[0].value = profile.userId;
+    }).catch(err => console.error(err));
+}
+liff.init({ liffId: "1656976284-2geMlQqP" }, () => {
+    if (liff.isLoggedIn()) {
+        runApp()
+    } else {
+        liff.login();
     }
-    liff.init({ liffId: "1656976287-9qzoegD1" }, () => {
-        if (liff.isLoggedIn()) {
-            runApp()
-        } else {
-            liff.login();
-        }
-    }, err => console.error(err.code, error.message));
+}, err => console.error(err.code, error.message));
+    // function runApp() {
+    //     liff.getProfile().then(profile => {
+    //         document.getElementById("pictureUrl").src = profile.pictureUrl;
+    //         document.getElementById("displayName").innerHTML = '<b>ชื่อผู้แจ้ง:</b> ' + profile.displayName;
+    //         document.getElementsByName("pname")[0].value = profile.displayName;
+    //         document.getElementsByName("id_user")[0].value = profile.userId;
+    //     }).catch(err => console.error(err));
+    // }
+    // liff.init({ liffId: "1656976287-9qzoegD1" }, () => {
+    //     if (liff.isLoggedIn()) {
+    //         runApp()
+    //     } else {
+    //         liff.login();
+    //     }
+    // }, err => console.error(err.code, error.message));
 </script>
 
     <script src="assets/jquery.min.js"></script>
